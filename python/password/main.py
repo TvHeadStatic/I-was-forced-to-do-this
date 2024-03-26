51eacca559a12cae02adc6d493dd7c97e5aee48b
Static <angiebusinessacc@gmail.com>
2024-03-26 09:35:54 +0100

    gay

--- python/password/main.py
+++ python/password/main.py
@@ -1,51 +1,51 @@
-def special(amongus):
+def num_char(ඞ):
+    uppercaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "1234567890"
+    sejf = 0
+    for x in uppercaseLetter:
+        if x in ඞ:
+            sejf += 1
+            break
+    for x in uppercaseLetter.lower():
+        if x in ඞ:
+            sejf += 1
+            break
+    for x in numbers:
+        if x in ඞ:
+            sejf += 1
+            break
+    return sejf
+
+def extraspeshl(ඞ):
     specials = "!?@#$%_-"
     for x in specials:
-        if x in amongus:
+        if x in ඞ:
             return 1
     return 0
 
-def num_char(amongus):
-    uppercaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    numbers = "1234567890"
-    safety = 0
-    for x in uppercaseLetter:
-        if x in amongus:
-            safety += 1
-            break
-    for x in uppercaseLetter.lower():
-        if x in amongus:
-            safety += 1
-            break
-    for x in numbers:
-        if x in amongus:
-            safety += 1
-            break
-    return safety
-
-def safety(amongus):
-    return special(amongus) + num_char(amongus) 
+def sejf(ඞ):
+    return num_char(ඞ) + extraspeshl(ඞ) 
 
 savedPasswords = []
-def similarity(amongus):
+def sims(ඞ):
     global savedPasswords
-    print(amongus)
+    print(ඞ)
     for x in savedPasswords:
-        if amongus == x:
+        if ඞ == x:
             return 1
-    savedPasswords.append(amongus)
+    savedPasswords.append(ඞ)
     return 0
 
 while(True):
     password = input("Password input: ")
     if len(password) >= 8:
-        print(f"Your password is infact usable good job :3 : {safety(password)}/5")
+        print(f"Your password is infact usable good job :3 : {sejf(password)}/5")
 
-        if similarity(password):
-            print("your password matches with another one (not saved)")
+        if sims(password):
+            print("Dumb b1- you can't resuse passwords (no saving for you)")
         else:
-            print("your password matches with neither of your passwords (saved hash)\n")
-        if input("Store a new password? [y/n]: ").upper() == 'n'.upper():
+            print("Holy- you are so creative good job (saved)\n")
+        if input("Ya want even more passwording ?! [y/n]").upper() == 'n'.upper():
             break
     else:
-        print("Password length must be atleast 8 characters long.")
+        print("Yo, you funny unlock word ain't long enough try again :3")
