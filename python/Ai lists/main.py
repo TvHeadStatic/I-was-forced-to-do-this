47bf79689267dcb4372b0cc30e5958e4d2199cdf
Static <angiebusinessacc@gmail.com>
2024-04-09 09:17:47 +0200

    gay

--- python/Ai lists/main.py
+++ python/Ai lists/main.py
@@ -1,3 +1,34 @@
 arr = ['@', 28, 8, 'HATSUNE MIKU', [96,69,6.9], 98069, 'Thats', 'just', 'a', 'THEORY']
 
 print(f"{arr[0]} {arr[int(len(arr)/2)-1]} {arr[int(len(arr)-1)]}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 1. Vytvořte seznam s různými druhy dat. (čísla, string, minimálně 10 různých hodnot).
+#2. Pomocí indexu vypište první, střední a poslední zadané hodnoty.
+#4. Sečtěte prvky seznamu a vypište jejich výsledek.
+#5. Vypište nejmenší a největší hodnotu v seznamu.
+#6. Pokud se v seznamu objevují stejná data ze seznamu je odstraňte a znovu vypište.
+#7. Vyměňte 3 hodnoty seznamu za libovolné hodnoty. (První, střední a poslední) - znovu vypište upravený seznam.
