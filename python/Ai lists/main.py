08d80176b60377396f6c557b3794105e3ae725e3
Static <angiebusinessacc@gmail.com>
2024-04-09 09:30:40 +0200

    gay

--- python/Ai lists/main.py
+++ python/Ai lists/main.py
@@ -2,6 +2,14 @@ arr = ['@', 28, 8, 'HATSUNE MIKU', [96,69,6.9], 98069, 'Thats', 'just', 'a', 'TH
 
 print(f"{arr[0]} {arr[int(len(arr)/2)-1]} {arr[int(len(arr)-1)]}")
 
+mEth = 0
+for i in range(len(arr)):
+    if type(arr[i]) is str:
+        for j in range(len(arr[i])):
+            mEth += ord(arr[i][j])
+    else:
+        mEth += arr[i]
+print(mEth)
 
 
 
@@ -26,9 +34,9 @@ print(f"{arr[0]} {arr[int(len(arr)/2)-1]} {arr[int(len(arr)-1)]}")
 
 
 
-# 1. Vytvořte seznam s různými druhy dat. (čísla, string, minimálně 10 různých hodnot).
+# 1. Vytvořte arr s různými druhy dat. (čísla, string, minimálně 10 různých hodnot).
 #2. Pomocí indexu vypište první, střední a poslední zadané hodnoty.
-#4. Sečtěte prvky seznamu a vypište jejich výsledek.
-#5. Vypište nejmenší a největší hodnotu v seznamu.
-#6. Pokud se v seznamu objevují stejná data ze seznamu je odstraňte a znovu vypište.
-#7. Vyměňte 3 hodnoty seznamu za libovolné hodnoty. (První, střední a poslední) - znovu vypište upravený seznam.
+#4. Sečtěte prvky arru a vypište jejich výsledek.
+#5. Vypište nejmenší a největší hodnotu v arru.
+#6. Pokud se v arru objevují stejná data ze arru je odstraňte a znovu vypište.
+#7. Vyměňte 3 hodnoty arru za libovolné hodnoty. (První, střední a poslední) - znovu vypište upravený arr.
